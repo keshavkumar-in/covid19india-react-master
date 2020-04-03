430f3f55af124f47c5e0524e4502bf5107dc62bb
kkeshav <14369793+kkeshav414@users.noreply.github.com>
2020-04-04 02:11:56 +0530

    css issue fixed

--- src/components/home.js
+++ src/components/home.js
@@ -100,7 +100,7 @@ function Home(props) {
           </React.Fragment>
         )}
         <div className="fadeInUp" style={{animationDelay: '0.5s'}}>
-          <h1>COVID-19 Symptom Checker</h1>
+          <h1 style={{marginLeft: '20px'}}>COVID-19 Symptom Checker</h1>
           <iframe
             id="hf-iframe"
             src="https://covid19.infermedica.com/en?header=false&guidelines_data=%7B%0A%20%20%22emergency_numbers%22%3A%20%5B%0A%20%20%20%20%7B%0A%20%20%20%20%20%20%22number%22%3A%20%22102%22%2C%0A%20%20%20%20%20%20%22description%22%3A%20%22Ambulance%22%0A%20%20%20%20%7D%2C%0A%20%20%20%20%7B%0A%20%20%20%20%20%20%22number%22%3A%20%22112%22%2C%0A%20%20%20%20%20%20%22description%22%3A%20%22National%20Emergency%20Number%22%0A%20%20%20%20%7D%0A%20%20%5D%2C%0A%20%20%22covid_numbers%22%3A%20%5B%0A%20%20%20%20%7B%0A%20%20%20%20%20%20%22number%22%3A%20%22%2B91-11-23978046%22%0A%20%20%20%20%7D%2C%0A%20%20%20%20%7B%0A%20%20%20%20%20%20%22number%22%3A%20%22%2B1075%22%2C%0A%20%20%20%20%20%20%22description%22%3A%20%22Toll%20Free%22%0A%20%20%20%20%7D%0A%20%20%5D%2C%0A%20%20%22websites%22%3A%20%5B%0A%20%20%20%20%7B%0A%20%20%20%20%20%20%22website%22%3A%20%22https%3A%2F%2Fwww.mohfw.gov.in%2F%22%2C%0A%20%20%20%20%20%20%22description%22%3A%20%22Ministry%20of%20Health%20and%20Family%20Welfare%22%0A%20%20%20%20%7D%2C%0A%20%20%20%20%7B%0A%20%20%20%20%20%20%22website%22%3A%20%22https%3A%2F%2Fwww.mohfw.gov.in%2Fpdf%2Fcoronvavirushelplinenumber.pdf%22%2C%0A%20%20%20%20%20%20%22description%22%3A%20%22State%20Wise%20Helpline%20Number%22%0A%20%20%20%20%7D%0A%20%20%5D%2C%0A%20%20%22free_text%22%3A%20%22Not%20sure%20what%20to%20do%3F%20Call%3A%20-%20%2B91-11-23978046%22%0A%7D"
